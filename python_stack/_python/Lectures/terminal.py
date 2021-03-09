09a2186510e1b478b73f94b60aab77bf93b6e089
Trey Beattie <beattietrey@yahoo.com>
2021-03-08 16:23:51 -0800

    Lectures

---
+++ python_stack/_python/Lectures/terminal.py
@@ -0,0 +1,8 @@
+def soft_serve(fName, flavor):
+    served = f'{fName} wants {flavor} ice cream!!!!'
+    # print(served)
+    return served
+
+
+treys_ice_cream = soft_serve('Trey', 'Rocky Road')
+print(treys_ice_cream)
