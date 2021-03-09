f23383535aad08fd1a8213ae408113edbd8b92e5
Trey Beattie <beattietrey@yahoo.com>
2021-03-09 11:42:24 -0800

    Finished Assignment

--- python_stack/_python/python_fundamentals/user.py
+++ python_stack/_python/python_fundamentals/user.py
@@ -5,12 +5,15 @@ class User:
 
     def make_deposit(self, amount):
         self.account_value+=amount
+        return self
     
     def make_withdrawal(self, amount):
         self.account_value-=amount
+        return self
     
     def display_user_balance(self):
         print(f'User: {self.name}, Balance: ${self.account_value}')
+        return self
 
     def transfer_money(self, other_user,amount):
         self.account_value-=amount
@@ -23,22 +26,10 @@ user1 = User("Guido van Rossum", 150)
 user2 = User("Jason Bateman", 1000)
 user3 = User("Shelley Peterson", 3000)
 
-user1.make_deposit(300)
-user1.make_deposit(300)
-user1.make_deposit(300)
-user1.make_withdrawal(500)
-user1.display_user_balance()
+user1.make_deposit(300).make_deposit(300).make_deposit(300).make_withdrawal(500).display_user_balance()
 
-user2.make_deposit(400)
-user2.make_deposit(20)
-user2.make_withdrawal(300)
-user2.make_withdrawal(300)
-user2.display_user_balance()
+user2.make_deposit(400).make_deposit(20).make_withdrawal(300).make_withdrawal(300).display_user_balance()
 
-user3.make_deposit(8000)
-user3.make_withdrawal(3000)
-user3.make_withdrawal(1000)
-user3.make_withdrawal(14000)
-user3.display_user_balance()
+user3.make_deposit(8000).make_withdrawal(3000).make_withdrawal(1000).make_withdrawal(14000).display_user_balance()
 
 user1.transfer_money(user3, 400)
