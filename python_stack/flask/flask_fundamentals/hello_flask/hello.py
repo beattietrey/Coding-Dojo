f97cf0c5d9380c3a58f27ccb11d6d46f76963b7d
Trey Beattie <beattietrey@yahoo.com>
2021-03-11 19:37:03 -0800

    Learn Platform

--- python_stack/flask/flask_fundamentals/hello_flask/hello.py
+++ python_stack/flask/flask_fundamentals/hello_flask/hello.py
@@ -3,11 +3,20 @@ app = Flask(__name__)    # Create a new instance of the Flask class called "app"
 
 @app.route('/')          # The "@" decorator associates this route with the function immediately following
 def index():
-    return render.template("index.html",phrase="hello",times=5) # Return the string 'Hello World!' as a response
+    return render_template("index.html",phrase="hello",times=5) # Return the string 'Hello World!' as a response
 
 @app.route('/success')
 def success():
     return "success"
 
+@app.route('/lists')
+def render_lists():
+    student_info = [
+        {'name' : 'Michael', 'age' : 35},
+        {'name' : 'John', 'age' : 30 },
+        {'name' : 'Mark', 'age' : 25},
+        {'name' : 'KB', 'age' : 27}
+    ]
+    return render_template("index.html", random_numbers = [3,1,5], students = student_info)
 if __name__=="__main__":   # Ensure this file is being run directly and not from a different module    
     app.run(debug=True)    # Run the app in debug mode.
