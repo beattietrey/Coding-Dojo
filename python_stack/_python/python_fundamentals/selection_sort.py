2d9b8d25404b5e5c7b4845ec9bf7cbee6a580de8
Trey Beattie <beattietrey@yahoo.com>
2021-03-14 19:14:38 -0700

    Finished Assignment

--- python_stack/_python/python_fundamentals/selection_sort.py
+++ python_stack/_python/python_fundamentals/selection_sort.py
@@ -1,19 +1,15 @@
 def selection_sort(li):
-    minIdx = 0 # what am I using this for?
-    for i in range(0,len(li),1): #starting at beginning of list
-        print(i)
-        min = li[i]
-        for j in (i, len(li)-1,1):
-            print(j)
-            if li[j] < min:
-                print(min)
-                min = li[j]
+    minIdx = 0
+    for i in range(0,len(li),1):
+        least = li[i]
+        for j in range(i+1,len(li),1):
+            if li[j] < li[i]:
+                least = li[j]
                 minIdx = j
-        temp = li[j]
-        li[j] = li[minIdx]
-        li[i] = temp
-        print(li)
+                li[minIdx] = li[i]
+                li[i] = least
     return li
 
 
-selection_sort([9,8,7,6,5,4,3,2,1])
+
+print(selection_sort([9,8,7,6,5,4,3,2,1]))
