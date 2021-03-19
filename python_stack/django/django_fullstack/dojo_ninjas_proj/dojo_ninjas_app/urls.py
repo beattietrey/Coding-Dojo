5d16e15ebdd0a0be9d836d5d5daca52d71459186
Trey Beattie <beattietrey@yahoo.com>
2021-03-18 18:28:03 -0700

    Couldn't get sensei

--- python_stack/django/django_fullstack/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ python_stack/django/django_fullstack/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
     
 urlpatterns = [
-    path('', views.index),	   
+    path('', views.index),
+    path('submit_dojo', views.dojo),
+    path('submit_ninja', views.ninja),
+    path('delete_dojo/<dojo_id>', views.delete_dojo),
+    
 ]
--- python_stack/django/django_fullstack/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ python_stack/django/django_fullstack/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from dojo_ninjas_app.models import *
 
 # Create your views here.
 def index(request):
-    pass
+    context={
+        'dojos': Dojo.objects.all(),
+    }
+
+    return render(request, 'index.html', context)
+
+def dojo(request):
+    Dojo.objects.create(
+        name=request.POST['name'],
+        city=request.POST['city'],
+        state=request.POST['state'],
+    )
+    return redirect('/')
+
+def ninja(request):
+    Ninja.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        dojo_id=Dojo.objects.get(id=request.POST['dojo_id']),
+    )
+    return redirect('/')
+
+def delete_dojo(request,dojo_id):
+    dojo=Dojo.objects.get(id=dojo_id)
+    dojo.ninjas.all().delete()
+    dojo.delete()
+    return redirect('/')
