2b3ef1dd28309dc4844d031b7d479779c43bcc54
Trey Beattie <beattietrey@yahoo.com>
2021-04-05 12:48:27 -0700

    FizzBuzz

---
+++ Java/JavaFun/fizzbuzz/FizzBuzzTest.java
@@ -0,0 +1,7 @@
+public class FizzBuzzTest {
+    public static void main(String[] args) {
+        FizzBuzz test = new FizzBuzz();
+        String output = test.fizzBuzz(4);
+        System.out.println(output);
+    }
+}
