a349fe59e24c2f76d1d055982db2c97fd0565777
Trey Beattie <beattietrey@yahoo.com>
2021-04-05 11:54:41 -0700

    First Day stuff

---
+++ Java/JavaFun/first_java_program/FirstJavaProgram.java
@@ -0,0 +1,12 @@
+public class FirstJavaProgram {
+    public static void main(String[] args) {
+        String myName = "Trey";
+        int age = 31;
+        String hometown = "Tacoma, WA";
+
+        System.out.println("My name is " + myName);
+        System.out.println("I am " + age + " years old");
+        System.out.println("My hometown is " + hometown);
+
+    }
+}
