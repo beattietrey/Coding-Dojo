1d819e9ecee5de67e67857a6eb5777b068a93778
Trey Beattie <beattietrey@yahoo.com>
2021-04-11 19:44:36 -0700

    Trie, all but printkeys

---
+++ Java/Tries/src/com/codingdojo/trey/tries/Trie.java
@@ -0,0 +1,69 @@
+package com.codingdojo.trey.tries;
+
+import java.util.Set;
+
+public class Trie {
+    public Node root;
+    
+    public Trie() {
+        this.root = new Node();
+    }
+    
+    public void insertWord(String word) {
+        // gets the root node;
+        Node currentNode = this.root;
+        
+        // iterates over every character in the word
+        for(int i = 0; i < word.length(); i++) {
+            // currentLetter is just the character / letter at the iteration
+            Character currentLetter = word.charAt(i);
+            // ask if the current letter is in the map of the current node
+            Node child = currentNode.children.get(currentLetter);
+            if(child == null) {
+                child = new Node();
+                currentNode.children.put(currentLetter, child); 
+            } 
+            
+            currentNode = child;
+        }
+        currentNode.isCompleteWord = true;
+    }
+    
+    public boolean isPrefixValid(String prefix) {
+    	Node current = this.root;
+    	
+    	for(int i = 0; i < prefix.length(); i++) {
+    		Character currentletter = prefix.charAt(i);
+    		Node child = current.children.get(currentletter);
+    		if(child == null) {
+    			return false;
+    		}
+    		current = child;
+    	}
+    	return true;
+    }
+    
+    
+    public boolean isWordValid(String word) {
+    	Node current = this.root;
+    	
+    	for(int i = 0; i < word.length(); i++) {
+    		Character currentletter = word.charAt(i);
+    		Node child = current.children.get(currentletter);
+    		if(child == null) {
+    			return false;
+    		}
+    		current = child;
+    	}
+    	return current.isCompleteWord;    
+    	}
+    
+//    public void printAllKeys() {
+//    	Node current = this.root;
+//    }
+//        Set<Character> keys = this.root.children.keySet();
+//        for(String key : keys) {
+//            System.out.println(key);
+//            System.out.println(userMap.get(key));   
+//    }
+}
