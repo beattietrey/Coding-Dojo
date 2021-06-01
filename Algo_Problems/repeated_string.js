c0cd21af3aa2498527e9d58812e49447ef6e26b9
Trey Beattie <beattietrey@gmail.com>
2021-05-31 18:46:14 -0700

    New Algos

---
+++ Algo_Problems/repeated_string.js
@@ -0,0 +1,17 @@
+// There is a string ,s , of lowercase English letters that is repeated infinitely many times. 
+// Given an integer, n , find and print the number of letter a's in the first  letters of the infinite string.
+
+const str1 = "aba"
+const len1 = 10
+
+const expectedOutput1 = 7
+
+const str2 = "a"
+const len1 = 1000000000000
+
+const expectedOutput1 = 1000000000000
+
+
+function repeatedString(s, n) {
+
+}
