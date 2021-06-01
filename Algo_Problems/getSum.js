96f21a0856ade236a4b5cc446b0bba784235be00
Trey Beattie <beattietrey@gmail.com>
2021-06-01 13:48:37 -0700

    Algo Problems

---
+++ Algo_Problems/getSum.js
@@ -0,0 +1,31 @@
+// Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+// Note: a and b are not ordered!
+
+// GetSum(1, 0) == 1   // 1 + 0 = 1
+// GetSum(1, 2) == 3   // 1 + 2 = 3
+// GetSum(0, 1) == 1   // 0 + 1 = 1
+// GetSum(1, 1) == 1   // 1 Since both are same
+// GetSum(-1, 0) == -1 // -1 + 0 = -1
+// GetSum(-1, 2) == 2  // -1 + 0 + 1 + 2 = 2
+
+
+
+function getSum( a,b ){
+    let sum = 0
+    if(a < b){
+        for(a;a <= b; a++){
+            sum+=a
+        }
+    } else if (b < a) {
+        for(b;b <= a; b++){
+            sum+=b
+        }
+    } else {
+        return a
+    }
+    return sum
+}
+
+
+console.log(getSum(20,-4))
