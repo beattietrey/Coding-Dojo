7cc545781ca70fd3a958bf21635335c78e90f11b
Trey-Thomas Beattie <beattietrey@gmail.com>
2021-09-04 13:26:28 -0700

    Create longestSubstringWithoutRepeat.js

---
+++ Algo_Problems/leetcodeProblems/longestSubstringWithoutRepeat.js
@@ -0,0 +1,20 @@
+// Given a string s, find the length of the longest substring without repeating characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3.
+
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+var lengthOfLongestSubstring = function(s) {
+    let output = 0;
+    for(let i = 0; i < s.length; i++){
+        
+    }
+}
