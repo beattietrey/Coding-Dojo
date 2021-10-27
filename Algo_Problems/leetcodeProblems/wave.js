d0f5d145b75fa0ff15ffe512ce685e687aa5d311
Trey-Thomas Beattie <beattietrey@gmail.com>
2021-10-26 20:22:13 -0700

    Coding Challenges

---
+++ Algo_Problems/leetcodeProblems/wave.js
@@ -0,0 +1,20 @@
+// In this simple Kata your task is to create a function that turns a string into a Mexican Wave. You will be passed a string and you must return that string in an array where an uppercase letter is a person standing up. 
+// Rules
+//  1.  The input string will always be lower case but maybe empty.
+
+//  2.  If the character in the string is whitespace then pass over it as if it was an empty seat
+// Example
+// wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str){
+    let arr = []
+    for(let i = 0; i < str.length; i++){
+      if(str[i] != " "){
+        let temp = str.slice(0,i)
+        temp+=str.charAt(i).toUpperCase()
+        temp+=str.slice(i+1)
+        arr.push(temp)
+      }
+    }
+    return arr
+  }
